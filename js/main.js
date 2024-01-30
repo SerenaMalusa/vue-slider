409ee99bf2c29dee49aebb7aaf176f5131901164
SerenaMalusa <sere.malu@gmail.com>
2024-01-30 15:51:51 +0100

    feat: write ternary operator directly in html

--- js/main.js
+++ js/main.js
@@ -4,16 +4,16 @@ const app = createApp({
     data() {
         return {
             imgNumbers : [1,2,3,4,5],
-            activeIndex : 2,
+            activeIndex : 3,
         }      
     },
 
-    methods: {
-        addActiveClass(currentIdex) {
-            let activeClass = currentIdex == this.activeIndex ? 'active':''; 
-            return activeClass;
-        }
-    }
+    // methods: {
+    //     addActiveClass(currentIdex) {
+    //         let activeClass = currentIdex == this.activeIndex ? 'active':''; 
+    //         return activeClass;
+    //     }
+    // }
 });
 
 app.mount('#app');
